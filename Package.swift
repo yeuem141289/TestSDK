5dd9328cea462acf4a3bcae4e118cb7c29bb7f3e
yeuem141289 <tuandang141289@gmail.com>
2025-08-13 10:55:36 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,13 +11,16 @@ let package = Package(
         .library(name: "TestSDK", type: .dynamic, targets: ["TestSDK"]), // type: .dynamic
     ],
     dependencies: [
+        // Khai báo dependency chính
         .package(url: "https://github.com/aws-amplify/amplify-ui-swift-liveness", from: "1.4.2"),
     ],
     targets: [
         .target(
             name: "TestSDK",
+            // Khai báo dependency cho target, trỏ đến product "FaceLiveness"
+            // trong package "amplify-ui-swift-liveness"
             dependencies: [
-                "FaceLiveness"
+                .product(name: "FaceLiveness", package: "amplify-ui-swift-liveness")
             ]
         )
 
